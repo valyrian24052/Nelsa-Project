58e131a1d8fb0cc96243f3803594046999d91034
Shashank Sharma <79395827+valyrian24052@users.noreply.github.com>
2025-07-02 12:58:33 +0000

    aaaa

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
-"use client"
+import { Header } from "../components/Header";
+import { HeroSection } from "../components/HeroSection";
+import { ServicesSection } from "../components/ServicesSection";
+import { ProjectsSection } from "../components/ProjectsSection";
+import { AboutSection } from "../components/AboutSection";
+import { ContactSection } from "../components/ContactSection";
+import { Footer } from "../components/Footer";
 
-import { Accordion } from "../components/ui/accordion"
-
-export default function SyntheticV0PageForDeployment() {
-  return <Accordion />
+export default function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Header />
+      <main>
+        <HeroSection />
+        <ServicesSection />
+        <ProjectsSection />
+        <AboutSection />
+        <ContactSection />
+      </main>
+      <Footer />
+    </div>
+  );
 }
